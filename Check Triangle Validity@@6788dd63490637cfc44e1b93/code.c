e2e17032dd438435cd7648f9dbf3543367a16ada
rhythm-wq <rhythm1615.becse24@chitkara.edu.in>
2025-02-06 10:53:36 +0530

    Solution to Check Triangle Validity

---
+++ Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if(a+b>c)
+    {
+        if(b+c>a)
+        {
+            if(c+a>b)
+            {
+                printf("Valid");
+            }
+            else
+            {
+                printf("Invalid");
+            }
+        }
+        else{
+            printf("Invalid");
+        }
+    }
+    else{
+        printf("Invalid");
+    }
+}
